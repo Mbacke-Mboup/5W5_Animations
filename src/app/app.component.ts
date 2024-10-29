0dc9ae10560c7d1e6030f5f3dbf43285ae9a1fff
brr <2274252@cegepmontpetit.ca>
2024-10-29 17:43:57 -0400

    Fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,52 @@
-import { Component } from '@angular/core';
-import {transition, trigger, useAnimation} from "@angular/animations";
+import { Component, OnInit } from '@angular/core';
+import { transition, trigger, useAnimation } from "@angular/animations";
+import { jackInTheBox, pulse, shakeX } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('death', [transition(':decrement', useAnimation(shakeX, { params: { timing: 0.5 } }))]),
+    trigger('attack', [transition(':increment', useAnimation(jackInTheBox, { params: { timing: 0.5, scale: 4.5 } }))]),
+
+  ]
 })
-export class AppComponent {
-  slimeIsPresent = false;
+export class AppComponent implements OnInit {
+  slimeIsPresent = 0;
+  slimeAttacks = 0;
+
+  ngOnInit(): void {
+  }
 
   constructor() {
   }
 
   spawn() {
-    this.slimeIsPresent = true;
-    // TODO Animation angular avec forwards
+    this.slimeIsPresent = 1;
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
   }
 
-  death(){
-    this.slimeIsPresent = false;
-    // TODO Animation angular avec forwards
-
-    // TODO 2e animation angular en même temps
+  death() {
+    this.slimeIsPresent = 0;
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 
-  attack(){
+  attack() {
     // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
+    this.slimeAttacks++;
     // TODO Jouer une autre animation avant
   }
 
-  hit(){
+  hit() {
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+  }
+
+  showSlime() {
+
   }
 }
